d6955afbfe15724c6ae23a299c2bb11a7f1d65d7
wangshaoyi <wangshaoyi@szhtxx.com>
2017-12-07 18:03:50 +0800

    555

--- demo-web/src/main/java/com/company/controller/DemoController.java
+++ demo-web/src/main/java/com/company/controller/DemoController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 public class DemoController {
     private static final Logger LOG= LoggerFactory.getLogger(DemoController.class);
 
-    @RequestMapping("sayHello333")
+    @RequestMapping("sayHello555")
     @ResponseBody
     public String sayHello(String name){
         LOG.info("前置参数name=>{}",name);
