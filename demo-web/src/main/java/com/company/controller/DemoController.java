9f200cfe63e764f645b1a3844751f7226190931d
wangshaoyi <wangshaoyi@szhtxx.com>
2017-12-07 17:56:59 +0800

    Merge branch 'master' of https://github.com/BAN-WANG/demo
    # Conflicts:
    #	demo-web/src/main/java/com/company/controller/DemoController.java

--- demo-web/src/main/java/com/company/controller/DemoController.java
+++ demo-web/src/main/java/com/company/controller/DemoController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 public class DemoController {
     private static final Logger LOG= LoggerFactory.getLogger(DemoController.class);
 
-    @RequestMapping("sayHello111")
+    @RequestMapping("sayHello222")
     @ResponseBody
     public String sayHello(String name){
         LOG.info("前置参数name=>{}",name);
