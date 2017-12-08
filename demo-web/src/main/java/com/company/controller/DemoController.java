b2e960620e5aefe363e560227110ffeecd650aa3
BAN-WANG <43888221@qq.com>
2017-12-08 11:18:49 +0800

    888
    88

--- demo-web/src/main/java/com/company/controller/DemoController.java
+++ demo-web/src/main/java/com/company/controller/DemoController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 public class DemoController {
     private static final Logger LOG= LoggerFactory.getLogger(DemoController.class);
 
-    @RequestMapping("sayHello666")
+    @RequestMapping("sayHello888")
     @ResponseBody
     public String sayHello(String name){
         LOG.info("前置参数name=>{}",name);
